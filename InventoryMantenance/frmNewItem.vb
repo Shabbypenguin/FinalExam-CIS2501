907b2a3d11493e378291f8eaf27e69e11ac03dec
ShabbyPenguin <ShabbyPenguin@gmail.com>
2017-08-26 22:16:16 -0400

    comment out validator code

--- InventoryMantenance/frmNewItem.vb
+++ InventoryMantenance/frmNewItem.vb
@@ -3,21 +3,23 @@
     ' Add a statement here that declares a public inventory item.
     Private Sub btnSave_Click(sender As Object,
     e As EventArgs) Handles btnSave.Click
-        If IsValidData() Then
-            ' Add code here that creates a new item
-            ' and closes the form.
-            Dim invItem = New InvItem(txtItemNo.Text, txtDescription.Text, txtPrice.Text)
+        'If IsValidData() Then
+        ' Add code here that creates a new item
+        ' and closes the form.
+
+        'We werent given validator file so i commented it out to allow it to compile.
+        Dim invItem = New InvItem(txtItemNo.Text, txtDescription.Text, txtPrice.Text)
             Me.Close()
-        End If
+        'End If
     End Sub
 
-    Private Function IsValidData() As Boolean
-        Return Validator.IsPresent(txtItemNo) AndAlso
-        Validator.IsInt32(txtItemNo) AndAlso
-        Validator.IsPresent(txtDescription) AndAlso
-        Validator.IsPresent(txtPrice) AndAlso
-        Validator.IsDecimal(txtPrice)
-    End Function
+    'Private Function IsValidData() As Boolean
+    '    Return Validator.IsPresent(txtItemNo) AndAlso
+    '    Validator.IsInt32(txtItemNo) AndAlso
+    '    Validator.IsPresent(txtDescription) AndAlso
+    '    Validator.IsPresent(txtPrice) AndAlso
+    '    Validator.IsDecimal(txtPrice)
+    'End Function
 
     Private Sub btnCancel_Click(sender As Object,
     e As EventArgs) Handles btnCancel.Click
