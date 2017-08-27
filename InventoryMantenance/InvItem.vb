34e83ed80b49375f109bb1ddc3357b9ef6b75ce8
ShabbyPenguin <ShabbyPenguin@gmail.com>
2017-08-26 21:52:18 -0400

    Barebones the class

--- InventoryMantenance/InvItem.vb
+++ InventoryMantenance/InvItem.vb
@@ -1,3 +1,47 @@
 ﻿Public Class InvItem
 
+    Public _itemno As Integer
+    Public _description As String
+    Public _price As Decimal
+
+    Property ItemNo() As Integer
+        Get
+
+        End Get
+        Set(value As Integer)
+
+        End Set
+    End Property
+
+    Property Description() As String
+        Get
+
+        End Get
+        Set(value As String)
+
+        End Set
+    End Property
+
+    Property Price() As Decimal
+        Get
+
+        End Get
+        Set(value As Decimal)
+
+        End Set
+    End Property
+
+    Function GetDisplayText(ByVal Output As String)
+        Output = _itemno.ToString + " " + _description + "(" + _price.ToString + ")."
+        Return Output
+    End Function
+
+    Sub New()
+
+    End Sub
+
+    Sub New(itemno, description, price)
+
+    End Sub
+
 End Class
